691d03fe951a9bc19be98985e288ca285c4e7cd2
gaiaunad <unad.si.gaia@gmail.com>
2021-01-11 19:36:19 -0500

    mensaje

---
+++ htdocs/iotcol/app/Controllers/Prueba.php
@@ -0,0 +1,20 @@
+<?php namespace App\Controllers;
+
+class Prueba extends BaseController
+{
+	public function index()
+	{
+		//return view('welcome_message');
+    echo "Esta es la Funcion Index";
+	}
+
+  public function test(){
+    echo "Esta es la Funcion Test";
+  }
+
+  public function test2(){
+    echo "Esta es la Funcion Test2";
+  }
+	//--------------------------------------------------------------------
+
+}
